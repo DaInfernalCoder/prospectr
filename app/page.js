1217486edf4244762528b904283359a005714085
mamun2519 <juborajislam46@gmail.com>
2024-12-30 10:09:36 +0600

    try code analysis

--- app/page.js
+++ app/page.js
@@ -17,9 +17,9 @@ import Header from "@/components/Header";
 export default function Page() {
   return (
     <>
-      <header className="p-4 flex justify-end max-w-7xl mx-auto">
+      {/* <header className="p-4 flex justify-end max-w-7xl mx-auto">
         <ButtonSignin text="Login" />
-      </header>
+      </header> */}
       <main>
         {/* <section className="flex flex-col items-center justify-center text-center gap-12 px-8 py-24">
           <h1 className="text-3xl font-extrabold">Ship Fast ⚡️</h1>
