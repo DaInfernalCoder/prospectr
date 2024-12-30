7f8cb68e52018d906e408ebe1a06467023077c64
mamun2519 <juborajislam46@gmail.com>
2024-12-30 10:12:27 +0600

    try code analysis

--- app/page.js
+++ app/page.js
@@ -64,7 +64,6 @@ export default function Page() {
         {/* <ButtonSupport /> */}
         {/* <CTA /> */}
         {/* <FeaturesListicle /> */}
-        <Modal isModalOpen={true} />
 
         <FAQ />
         <Footer />
